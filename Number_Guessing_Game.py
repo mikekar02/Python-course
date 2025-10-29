36341aa08b665e3d31bb3f6a181a3226cf9dcfd7
Mike Karvelis <mikekar02@gmail.com>
2025-10-29 15:15:14 +0200

    added life mechanic

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,13 @@
+def set_lives(dif):
+    if dif == "easy":
+        return 10
+    elif dif == "hard":
+        return 5
+
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+dif = input("Choose a difficulty. Type 'easy' or 'hard'.").lower() #set the difficulty
+
+lives = set_lives(dif)
+print(lives)
