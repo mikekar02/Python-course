1f4db80b4173e367170163f7ee3fb95f73d11afc
Mike Karvelis <mikekar02@gmail.com>
2025-11-03 11:39:08 +0200

    added coffee resource check

--- coffee_machine.py/main.py
+++ coffee_machine.py/main.py
@@ -28,5 +28,36 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0
 }
+machine = True
+coffee = True
 
+
+while machine == True:
+    ans = input("What would you like? (espresso/latte/cappuccino):").lower() #1 Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+
+    if ans != "off": #2 Turn off the Coffee Machine by entering “off” to the prompt.
+
+        if ans == "report":
+            print("Water: \n",resources["water"])
+            print("Milk: \n",resources["milk"])
+            print("Coffee: \n",resources["coffee"])
+            print("Money: \n",resources["money"]) #3 Print report
+
+    if ans == "latte":
+        if resources["water"] < MENU["latte"]["ingredients"]["water"]:
+            print("Sorry there is not enough water.")
+            coffee = False
+        elif resources["milk"] < MENU["latte"]["ingredients"]["milk"]:
+            print("Sorry there is not enough milk.")
+            coffee = False
+        elif resources["coffee"] < MENU["latte"]["ingredients"]["coffee"]:
+            print("Sorry there is not enough coffee.")
+            coffee = False #4 Check resources sufficient?
+
+
+
+#5 Process coins.
+#6 Check transaction successful?
+#7 Make Coffee.
